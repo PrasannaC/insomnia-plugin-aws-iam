22d6c729d31880f8e7fbdd93a418642a2e65aa80
Prasann Choudhari <choulinux@gmail.com>
2023-07-31 16:48:23 +0000

    Extract region from aws credentials.

--- index.js
+++ index.js
@@ -3,31 +3,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const util_1 = require("util");
 const awscred = require('awscred');
 const loadAwsCred = util_1.promisify(awscred.load);
-var Attribute;
-(function (Attribute) {
-    Attribute["accessKeyId"] = "accessKeyId";
-    Attribute["secretAccessKey"] = "secretAccessKey";
-})(Attribute || (Attribute = {}));
+const loadRegion = util_1.promisify(awscred.loadCredentialsAndRegion);
+var CredentialAttribute;
+(function (CredentialAttribute) {
+    CredentialAttribute["accessKeyId"] = "accessKeyId";
+    CredentialAttribute["secretAccessKey"] = "secretAccessKey";
+    CredentialAttribute["sessionToken"] = "sessionToken";
+    CredentialAttribute["region"] = "region";
+})(CredentialAttribute || (CredentialAttribute = {}));
 exports.templateTags = [
     {
-        name: 'awsiam',
-        displayName: 'awsiam',
-        description: 'Insomnia plugin - AWS IAM credential loader',
+        name: 'awscreds',
+        displayName: 'awscreds',
+        description: 'Plugin: AWS IAM credential loader',
         args: [{
                 displayName: 'Attribute',
                 type: 'enum',
                 options: [
                     {
-                        displayName: Attribute.accessKeyId,
-                        value: Attribute.accessKeyId,
+                        displayName: CredentialAttribute.accessKeyId,
+                        value: CredentialAttribute.accessKeyId,
                     },
                     {
-                        displayName: Attribute.secretAccessKey,
-                        value: Attribute.secretAccessKey,
+                        displayName: CredentialAttribute.secretAccessKey,
+                        value: CredentialAttribute.secretAccessKey,
                     },
+                    {
+                        displayName: CredentialAttribute.sessionToken,
+                        value: CredentialAttribute.sessionToken,
+                    },
+                    {
+                        displayName: CredentialAttribute.region,
+                        value: CredentialAttribute.region,
+                    }
                 ]
             }],
         async run(context, attribute) {
+            if (attribute == CredentialAttribute.region) {
+                return awscred.loadRegionSync();
+            }
             const loadedCredentialObject = await loadAwsCred();
             return loadedCredentialObject.credentials[attribute];
         },
--- index.ts
+++ index.ts
@@ -1,38 +1,49 @@
 import { promisify } from 'util';
 const awscred = require('awscred');
 
-const loadAwsCred = promisify(awscred.load);
 
-enum Attribute {
+
+const loadAwsCred = promisify(awscred.load);
+const loadRegion = promisify(awscred.loadCredentialsAndRegion);
+
+enum CredentialAttribute {
     accessKeyId = 'accessKeyId',
     secretAccessKey = 'secretAccessKey',
     sessionToken = 'sessionToken',
+    region = 'region',
 }
 
 export const templateTags = [
     {
         name: 'awscreds',
         displayName: 'awscreds',
-        description: 'Insomnia plugin - AWS IAM credential loader',
+        description: 'Plugin: AWS IAM credential loader',
         args: [{
             displayName: 'Attribute',
             type: 'enum',
             options: [
                 {
-                    displayName: Attribute.accessKeyId,
-                    value: Attribute.accessKeyId,
+                    displayName: CredentialAttribute.accessKeyId,
+                    value: CredentialAttribute.accessKeyId,
                 },
                 {
-                    displayName: Attribute.secretAccessKey,
-                    value: Attribute.secretAccessKey,
+                    displayName: CredentialAttribute.secretAccessKey,
+                    value: CredentialAttribute.secretAccessKey,
                 },
                 {
-                    displayName: Attribute.sessionToken,
-                    value: Attribute.sessionToken,
+                    displayName: CredentialAttribute.sessionToken,
+                    value: CredentialAttribute.sessionToken,
+                },
+                {
+                    displayName: CredentialAttribute.region,
+                    value: CredentialAttribute.region,
                 }
             ]
         }],
-        async run(context: object, attribute: Attribute) {
+        async run(context: object, attribute: CredentialAttribute) {
+            if (attribute == CredentialAttribute.region){
+                return awscred.loadRegionSync();
+            }
             const loadedCredentialObject = await loadAwsCred();
             return loadedCredentialObject.credentials[attribute];
         },
